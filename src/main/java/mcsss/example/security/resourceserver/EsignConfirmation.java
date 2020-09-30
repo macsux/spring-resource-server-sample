7509695a1a78d56dee3c5f924d44054c031f559c
Andrew Stakhov <andrew.stakhov@gmail.com>
2020-09-30 11:26:13 -0400

    Add echo for callback

---
+++ src/main/java/mcsss/example/security/resourceserver/EsignConfirmation.java
@@ -0,0 +1,11 @@
+package mcsss.example.security.resourceserver;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class EsignConfirmation {
+    String app;
+    String docId;
+}
--- src/main/java/mcsss/example/security/resourceserver/EsignController.java
+++ src/main/java/mcsss/example/security/resourceserver/EsignController.java
@@ -3,6 +3,7 @@ package mcsss.example.security.resourceserver;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class EsignController {
     @PostMapping(value = "/complete_signing")
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasAuthority('SCOPE_esignature.complete_signing')")
-    public String callback() {
-        return "test";
+    public EsignConfirmation callback(@RequestParam String app, @RequestParam String docId) {
+        return new EsignConfirmation(app, docId);
     }
 }
